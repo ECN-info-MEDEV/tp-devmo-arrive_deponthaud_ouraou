04ed1e5650ebeec7d2864ca4b27590c3fe0c98aa
ElsaArr <elsa.arrive.34@gmail.com>
2024-03-23 12:12:47 +0100

    Essaie liste d'amis

---
+++ app/src/main/java/com/example/instagranny/data/DataSource.kt
@@ -0,0 +1,11 @@
+package com.example.instagranny.data
+
+object DataSource {
+    val paramAmis = listOf(
+        Pair(com.example.instagranny.R.string.profil1, com.example.instagranny.R.drawable.profil1),
+        Pair(com.example.instagranny.R.string.profil2, com.example.instagranny.R.drawable.profil2),
+        Pair(com.example.instagranny.R.string.profil3, com.example.instagranny.R.drawable.profil3),
+        Pair(com.example.instagranny.R.string.profil4, com.example.instagranny.R.drawable.profil4),
+        Pair(com.example.instagranny.R.string.profil5, com.example.instagranny.R.drawable.profil5)
+    )
+}
